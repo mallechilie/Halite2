2787e2c8f010d12690c6e248a7a0cb3ac43b14c3
mallechilie <michiel.doorn@gmail.com>
2017-11-15 23:53:29 +0100

    moar logging

--- MyBot.cs
+++ MyBot.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Text;
 using Halite2.hlt;
 
@@ -10,15 +11,22 @@ namespace Halite2
 
 		public static void Main(string[] args)
 		{
-			//Trace.Listeners.Add(new TextWriterTraceListener(string.Format("Halite.{0}.text.log", System.AppDomain.CurrentDomain.FriendlyName)));
-			//Trace.Listeners.Add(new EventLogTraceListener(string.Format("Halite.{0}.event.log", System.AppDomain.CurrentDomain.FriendlyName)));
+			//Trace.Listeners.Add(new TextWriterTraceListener(string.Format("Halite.{0}.text.log", AppDomain.CurrentDomain.FriendlyName)));
+			//Trace.Listeners.Add(new EventLogTraceListener(string.Format("Halite.{0}.event.log", AppDomain.CurrentDomain.FriendlyName)));
 			//Trace.Listeners.Add(new ConsoleTraceListener());
 			//Trace.AutoFlush = true;
-			//Trace.TraceError("msg");
 			Collision.CircleIntersectNewPoint(new Position(100, 0), new Position(100, 10), new Entity(0, 0, 97, 7, 100, 5), 0);
-			world = new World(args);
-			while (true)
-				Networking.SendMoves(world.DoTurn());
+			try
+			{
+				world = new World(args);
+				while (true)
+					Networking.SendMoves(world.DoTurn());
+			}
+			catch (Exception ex)
+			{
+				DebugLog.AddLog("Message :" + ex.Message + Environment.NewLine + "StackTrace :" + ex.StackTrace + "" + Environment.NewLine + "Date :" + DateTime.Now);
+				//Trace.TraceError("Message :" + ex.Message + Environment.NewLine + "StackTrace :" + ex.StackTrace + "" + Environment.NewLine + "Date :" + DateTime.Now);
+			}
 		}
 	}
 }
